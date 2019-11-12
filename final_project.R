40624ca001a35737dcae60e02bfed108d7f715b6
Administrator <alexzhou0330@163.com>
2019-11-12 14:03:01 +0800

    final project

--- final_project.R
+++ final_project.R
@@ -8,22 +8,7 @@ library(plotly)
 library(purrr)
 library(quantmod)
 library(treemapify)
-library(RColorBrewer)
-#期货主力合约数据
-ZZ_pnl <- dbGetQuery(mydb,"select trade_date,trade_code,price_change_pct from cn_future.daily_quote where trade_code in (\"SF912\",\"AP912\",\"CJ912\",\"ZC912\") order by trade_date desc;")
-DL_pnl <- dbGetQuery(mydb,"select trade_date,trade_code,price_change_pct from cn_future.daily_quote where trade_code in (\"I1912\",\"M1912\",\"JD1912\",\"Y1912\") order by trade_date desc;")
-SH_pnl <- dbGetQuery(mydb,"select trade_date,trade_code,price_change_pct from cn_future.daily_quote where trade_code in (\'RB1912\',\'AG1912\',\'FU1912\',\'PB1912\') order by trade_date desc;")
-ZZ_pnl <- reshape(ZZ_pnl, idvar = "trade_date", timevar = "trade_code", direction = "wide")
-DL_pnl <- reshape(DL_pnl, idvar = "trade_date", timevar = "trade_code", direction = "wide")
-SH_pnl <- reshape(SH_pnl, idvar = "trade_date", timevar = "trade_code", direction = "wide")
-ZZ_pnl <- xts(ZZ_pnl[,-1],as.Date(ZZ_pnl$trade_date))
-DL_pnl <- xts(DL_pnl[,-1],as.Date(DL_pnl$trade_date))
-SH_pnl <- xts(SH_pnl[,-1],as.Date(SH_pnl$trade_date))
-names(ZZ_pnl) <-  unlist(strsplit(names(ZZ_pnl),'.',fixed = TRUE))[c(2,4,6,8)]
-names(DL_pnl) <-  unlist(strsplit(names(DL_pnl),'.',fixed = TRUE))[c(2,4,6,8)]
-names(SH_pnl) <-  unlist(strsplit(names(SH_pnl),'.',fixed = TRUE))[c(2,4,6,8)]
-
-
+library(ggpubr)
 
 
 ## Get Tushare Data
@@ -52,17 +37,28 @@ row.names(ind_returns)<- c("CSI 300 Energy","CSI 300 Materials","CSI 300 Industr
 SSE50_constituents <- read.csv("C:/Users/Administrator/Desktop/final_project/000016closeweight.csv")
 SSE50_codes <- paste0(SSE50_constituents$Constituent.Code,".SH")
 SSE50_names <- SSE50_constituents$Constituent.Name
+SSE50_basics <- data.frame()
 SSE50 <- data.frame()
 for (code in SSE50_codes){
-  stk <- tushare(api_name = "daily", ts_code = code, trade_date = return_cal_dates[2])
-  stk_mv <- tushare(api_name = "daily_basic", ts_code = code,  trade_date = return_cal_dates[2])
+  stk <- tushare(api_name = "daily", ts_code = code, trade_date = return_cal_dates[1])
+  stk_mv <- tushare(api_name = "daily_basic", ts_code = code,  trade_date = return_cal_dates[1])
   SSE50[code,"returns"] <- stk$close/stk$pre_close -1 
+  SSE50[code,"vol"] <- stk$vol
   SSE50[code,"mv"] <- stk_mv$total_mv
+  SSE50_basics <- rbind(SSE50_basics,stk_mv)
 }
 SSE50$names <- paste0(SSE50_names,'\n',round(SSE50$returns*100,2),'%')
-
+SSE50_basics_new <- data.frame(row.names = 1:50)
+SSE50_basics_new$Code <- SSE50_codes
+SSE50_basics_new$Company <- SSE50_names
+SSE50_basics_new$Exchange <- SSE50_constituents$Exchange
+SSE50_basics_new["Market Cap"] <- SSE50_basics$total_mv
+SSE50_basics_new["P/E"] <- SSE50_basics$pe
+SSE50_basics_new$Price <- SSE50_basics$close
+SSE50_basics_new$Change <- paste0(round(SSE50$returns*100,2),"%")
+SSE50_basics_new$Volume <- SSE50$vol
 ###Draw figures
-## plot candlestick
+## Plot candlestick
 SSE_Index <- tail(SSE_Index,50)
 SSE_candlestick <- SSE_Index %>%
   plot_ly(x = ~trade_date, type="candlestick",
@@ -70,13 +66,13 @@ SSE_candlestick <- SSE_Index %>%
           high = ~high, low = ~low) %>%
   layout(title = "SSE Composite Index")
 SSE_candlestick
-## plot industry indices barchart
+## Plot industry indices barchart
 to_color <- function (x){
   if (x > 0){
-    return(sprintf("rgb(%d,0,0)",floor(255*(1-exp(-x*100)))));
+    return(sprintf("rgb(%d,0,0)",floor(255*(1-exp(-x*30)))));
   }
   else if (x<=0){
-    return(sprintf("rgb(0,%d,0)",ceiling(255*(1-exp(x*100)))));
+    return(sprintf("rgb(0,%d,0)",ceiling(255*(1-exp(x*30)))));
   }
 }
 ind_returns["color"] = map_chr(ind_returns$returns, to_color)
@@ -92,7 +88,7 @@ ind_indices_barchart <- plot_ly(y = reorder(row.names(ind_returns), ind_returns$
 
 ind_indices_barchart
 
-## plot SSE50 constituents maps
+## Plot SSE50 constituents maps
 rgb2hex <- function(rgb){
   rgb <- strsplit(substr(rgb,5,nchar(rgb)-1),',')[[1]]
   rgb <- as.integer(rgb)
@@ -109,5 +105,88 @@ SSE50["color"] = map_chr(SSE50$returns, to_color) %>% map_chr(rgb2hex)
 ggplot(SSE50, aes(area = mv, label = names)) +
   geom_treemap(fill = SSE50$color) +
   geom_treemap_text(fontface = "bold", colour = "white", place = "centre",
-                    grow = TRUE)
+                    grow = TRUE) + 
+  ggtitle("上证50成分股涨跌情况")
+
+## Generate the screener overview
+SSE50_basics_new <- head(SSE50_basics_new,5)
+tbody.style = tbody_style(color = "black",
+                          fill = c("#bdbdbd","#ededed"), hjust=1, x=0.9)
+ggtexttable(SSE50_basics_new,
+            theme = ttheme(
+              colnames.style = colnames_style(color = "white", fill = "#7d7d7d"),
+              tbody.style = tbody.style
+            ))
+## Generate the line graph of individual stock
+PingAn <- tushare(api_name = "daily", ts_code = '000001.SZ', start_date = '20190405', end_date = return_cal_dates[1])
+PingAn_return = PingAn$close[nrow(PingAn)]/PingAn$pre_close[nrow(PingAn)] - 1 
+
+PingAn_name <- list(
+          xref = 'paper',
+          yref = 'paper',
+          x = 0.23,
+          y = 0.99,
+          xanchor = 'right',
+          yanchor = 'middle',
+          text = '平安银行\n',
+          font = list(family = '楷体',
+                      size = 20,
+                      color = '#efefef'),
+          showarrow = FALSE)
+PingAn_price <- list(
+  xref = 'paper',
+  yref = 'paper',
+  x = 0.20,
+  y = 0.85,
+  xanchor = 'right',
+  yanchor = 'middle',
+  text = ~as.character(PingAn$close[nrow(PingAn)]),
+  font = list(family = 'Arial',
+              size = 26,
+              color = '#efefef'),
+  showarrow = FALSE)
+
+PingAn_change <- list(  
+        xref = 'paper',
+        yref = 'paper',
+        x = 0.84,
+        y = 0.98,
+        xanchor = 'right',
+        yanchor = 'middle',
+        text = ~paste0(round(PingAn_return*100,2),'%'),
+        font = list(family = 'Arial',
+                    size = 20,
+                    color = '#efefef'),
+        showarrow = FALSE)
+
+PingAn_HL <- list(  
+  xref = 'paper',
+  yref = 'paper',
+  x = 0.84,
+  y = 0.80,
+  xanchor = 'right',
+  yanchor = 'middle',
+  text = ~paste0("H ",PingAn$high[nrow(PingAn)],"\nL ",PingAn$low[nrow(PingAn)]),
+  font = list(family = 'Arial',
+              size = 16,
+              color = '#cfcfcf'),
+  showarrow = FALSE)
+
+PingAn_plot <- plot_ly(PingAn, y=~close,x=~paste0(substr(trade_date,5,6),'-',substr(trade_date,7,8)),
+                       type = 'scatter', mode = 'lines',
+                       line = list(color = '#adadad')) %>%
+              layout(paper_bgcolor=to_color(PingAn_return), plot_bgcolor=to_color(PingAn_return),
+                     xaxis = list(title = ""),
+                     yaxis = list(title = "", range=c(min(PingAn$close),max(PingAn$close)*1.5 - 0.5*min(PingAn$close))),
+                     margin =list(autoexpand = TRUE, r=10,l=10))%>%
+              layout(annotations = PingAn_name)%>%
+              layout(annotations = PingAn_price)%>%
+              layout(annotations = PingAn_change)%>%
+              layout(annotations = PingAn_HL)
+PingAn_plot
+
+
+
+
+
 
